c65e4dc17950e0c7fcddbf17f05a5ba3e46dcafb
Darren Wayn <darrenwayn98@gmail.com>
2022-09-22 21:09:53 +0700

    started input auto suggestion feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import { useState, useEffect } from "react";
 import CardList from "./components/card-list/CardList";
 import SearchBox from "./components/search-box/SearchBox";
+import AutoSuggest from "./components/auto-suggestion/AutoSuggest";
 import "./App.css";
 
 function App() {
   const [monsters, setMonsters] = useState([]);
+  const [suggestion, setSuggestion] = useState([]);
   const [searchField, setSearchField] = useState("");
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => setMonsters(users));
+      .then((users) => setMonsters(users))
+    
+    setSuggestion(monsters)
   }, []);
-
-  const handleChange = (e) => {
-    const onSearch = e.target.value
-    setSearchField(onSearch.toLocaleLowerCase());
-  };
 
   const filteredMonster = monsters.filter((monster) =>
     monster.name.toLocaleLowerCase().includes(searchField)
   );
+
+  const handleChange = (e) => {
+    const onSearch = e.target.value;
+    setSearchField(onSearch.toLocaleLowerCase());
+  };
 
   return (
     <div className="App">
@@ -31,6 +35,7 @@ function App() {
         className="search-box"
         type="search"
       />
+      {searchField ? <AutoSuggest /> : null}
       <CardList monsters={filteredMonster} searchField={searchField} />
     </div>
   );
---
+++ src/components/auto-suggestion/AutoSuggest.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './AutoSuggest.css'
+
+const AutoSuggest = () => {
+  return (
+    <div className="container-suggestion">
+        <div className='suggestion'>test</div>
+    </div>
+  );
+};
+
+export default AutoSuggest;
